76dc39be1a00e5e4aa719f8ebae8fe12fe0ea58a
leejooheon <leejooheon@naver.com>
2024-12-21 10:39:28 +0900

    Feat: ocr 동시접근하면 안되는듯.. mutex로 막자

--- src/main/kotlin/follower/ocr/TextDetecter.kt
+++ src/main/kotlin/follower/ocr/TextDetecter.kt
@@ -7,34 +7,42 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import net.sourceforge.tess4j.Tesseract
 import java.awt.Rectangle
 
-class TextDetecter {
+object TextDetecter {
     private val tesseract = Tesseract().apply {
         setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata")
         setLanguage("kor")
     }
 
+    private val mutex = Mutex()
+
     suspend fun detect(
         targets: List<String>,
         rectangle: Rectangle
     ): Boolean = withContext(Dispatchers.IO) {
-        val image = Keyboard.capture(rectangle)
-        try {
-            val origin = tesseract.doOCR(image.toAwtImage())
-            return@withContext targets.contains(origin)
-        } catch (e: Exception) {
-            return@withContext false
+        mutex.withLock {
+            val image = Keyboard.capture(rectangle)
+            try {
+                val origin = tesseract.doOCR(image.toAwtImage())
+                return@withContext targets.contains(origin)
+            } catch (e: Exception) {
+                return@withContext false
+            }
         }
     }
 
     suspend fun detectString(
         rectangle: Rectangle
     ): String = withContext(Dispatchers.IO) {
-        val image = Keyboard.capture(rectangle)
-        val origin = tesseract.doOCR(image.toAwtImage())
-        return@withContext origin
+        mutex.withLock {
+            val image = Keyboard.capture(rectangle)
+            val origin = tesseract.doOCR(image.toAwtImage())
+            return@withContext origin
+        }
     }
 }
