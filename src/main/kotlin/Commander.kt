f3847cf3983a5916bf710e1ddfe201d1861b9dfc
Jooheon <jooheon.lee@bugs.co.kr>
2024-12-30 09:41:55 +0900

    windowState 변경

--- src/main/kotlin/Commander.kt
+++ src/main/kotlin/Commander.kt
@@ -1,16 +1,13 @@
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.*
 import commander.CommanderApp
 import common.event.RegisterNativeHook
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        state = WindowState(
+        state = rememberWindowState(
             size = DpSize(450.dp, 450.dp),
             position = WindowPosition.PlatformDefault
         )
--- src/main/kotlin/Follower.kt
+++ src/main/kotlin/Follower.kt
@@ -1,16 +1,13 @@
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.*
 import common.event.RegisterNativeHook
 import follower.FollowerApp
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        state = WindowState(
+        state = rememberWindowState(
             size = DpSize(500.dp, 700.dp),
             position = WindowPosition.PlatformDefault
         )
